3081474e33427454b9bbcb1be8ab0a439ad94d47
amitco123 <amitco654321@gmail.com>
2022-10-27 09:29:11 +0300

    FirstUpload

---
+++ app/src/main/java/com/example/myfinalproject/MainActivity.java
@@ -0,0 +1,48 @@
+package com.example.myfinalproject;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
+
+public class MainActivity extends AppCompatActivity {
+    BottomNavigationView BottomNavigationView;
+    HomeFragment homeFragment= new HomeFragment();
+    AddFragment addFragment=new AddFragment();
+    GraphFragment graphFragment=new GraphFragment();
+    UpdateFragment updateFragment=new UpdateFragment();
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        BottomNavigationView= findViewById(R.id.bottom_navigation);
+        getSupportFragmentManager().beginTransaction().replace(R.id.container,homeFragment).commit();
+        BottomNavigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected( MenuItem item) {
+            switch (item.getItemId()){
+                case R.id.home:
+                    getSupportFragmentManager().beginTransaction().replace(R.id.container,homeFragment).commit();
+                    return true;
+                case R.id.add:
+                    getSupportFragmentManager().beginTransaction().replace(R.id.container,addFragment).commit();
+                    return true;
+                case R.id.graph:
+                    getSupportFragmentManager().beginTransaction().replace(R.id.container,graphFragment).commit();
+                    return true;
+                case R.id.update:
+                    getSupportFragmentManager().beginTransaction().replace(R.id.container,updateFragment).commit();
+                    return true;
+            }
+
+
+                return false;
+            }
+        });
+        }
+        }
+
