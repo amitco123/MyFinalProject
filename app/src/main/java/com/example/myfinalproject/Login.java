2b42f9f562da22b4a877fb2ce4d7f543ce0898c8
amitco123 <amitco654321@gmail.com>
2022-11-10 09:31:10 +0200

    loginpage

--- app/src/main/java/com/example/myfinalproject/Login.java
+++ app/src/main/java/com/example/myfinalproject/Login.java
@@ -12,10 +12,6 @@ public class Login extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        ConstraintLayout constraintLayout =findViewById(R.id.login_layout);
-        AnimationDrawable animationDrawable= (AnimationDrawable) constraintLayout.getBackground();
-        animationDrawable.setEnterFadeDuration(2500);
-        animationDrawable.setExitFadeDuration(5000);
-        animationDrawable.start();
+
     }
 }
